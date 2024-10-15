3aead61d5e5a875d2edd1856ca38e3d63eb434b0
Iury Sávio Pinheiro <iurysaviopinheiro@gmail.com>
2024-10-15 08:21:12 -0300

    data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,8 +1,12 @@
 import os
 import sys
+import pandas as pd
+
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.utils import save_object
+
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -20,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info('Data ingestion started!')
         try:
-            df = pd.read_csv('notebooks\data\stud.csv')
+            df = pd.read_csv('notebooks\data\stud.csv') # Can be changed based on the datasource that we'll work on. 
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok= True)
@@ -41,6 +45,9 @@ class DataIngestion:
             raise CustomException(e, sys)
         
 
-if __name__ =="__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
